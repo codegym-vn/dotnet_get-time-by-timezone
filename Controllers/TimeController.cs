461f56acd5ba2d279b9d5bfc952d0b5413451ee8
The Nguyen Khac <the.nguyen@codegym.vn>
2020-04-27 20:22:57 +0700

    get time by timezone

---
+++ Controllers/TimeController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TimeZone.Models;
+namespace TimeZone.Controllers
+{
+    public class TimeController : Controller
+    {
+        [HttpGet("/worldclock")]
+        public IActionResult GetTimeByTimezone(string timezone)
+        {
+            DateTime date = DateTime.Now;
+            DateTime destinationDate = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(date, timezone);
+            return View(destinationDate);
+        }
+
+            [HttpGet("/clock")]
+            public IActionResult GetTime()
+            {
+                DateTime date = DateTime.Now;
+                return View(date);
+            }
+    }
+}
